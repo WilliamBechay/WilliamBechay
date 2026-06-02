a38c1c0790c5f7bd947ba47b0be104c1b620fc06
William Bechay <72413042+WilliamBechay@users.noreply.github.com>
2026-06-02 13:32:08 -0400

    Update customSupabaseClient.js

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gxzicdjsyxsaxhsqdfko.supabase.co';
-const supabaseAnonKey = 'sb_publishable_YH3VurNFDsKExRaIb_4Ijg_yjL2RCxG';
+const supabaseUrl = 'https://nkiigfjmzuqylrsggpsn.supabase.co';
+const supabaseAnonKey = 'sb_publishable_japAtzl88_5JhV2OLwtw6w_6Jqbe3MC';
 
 const customSupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
